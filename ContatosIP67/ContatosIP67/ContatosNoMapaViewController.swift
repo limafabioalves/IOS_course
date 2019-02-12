28a0531882e0c75f5e6cce9f936a019da141fea6
ios8207 <ios8207@mac-mini-de-caelum-87.local>
2019-02-11 22:32:15 -0200

    trabalhando com mapa, mostrando localização e foto

--- ContatosIP67/ContatosIP67/ContatosNoMapaViewController.swift
+++ ContatosIP67/ContatosIP67/ContatosNoMapaViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import MapKit
 
-class ContatosNoMapaViewController: UIViewController {
+class ContatosNoMapaViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapa: MKMapView!
+    
+    var contatos: [Contato] = Array()
+    let dao:ContatoDao = ContatoDao.sharedInstance()
     
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.mapa.delegate = self
         
         self.locationManager.requestWhenInUseAuthorization()
         
@@ -26,10 +31,47 @@ class ContatosNoMapaViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil;
+        }
+        
+        let identifier: String = "pino"
+        var pino:MKPinAnnotationView
+        
+        if let reusablePin = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+            pino = reusablePin
+        } else {
+            pino = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        if let contato = annotation as? Contato{
+            pino.pinTintColor = UIColor.red
+            pino.canShowCallout = true
+            
+            let frame = CGRect(x: 0.0, y: 0.0, width: 32.0, height: 32.0)
+            let imagemContato = UIImageView(frame: frame)
+            
+            imagemContato.image = contato.foto
+            pino.leftCalloutAccessoryView = imagemContato
+        }
+        
+        return pino
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.contatos = dao.listaTodos()
+        self.mapa.addAnnotations(self.contatos)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.mapa.removeAnnotations(self.contatos)
     }
     
 
